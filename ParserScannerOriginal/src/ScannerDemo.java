fee5d40734c2ae7491f778e9d056e2457f8e7dc7
Yevgeniy <yshatrovskiy@gmail.com>
2017-11-14 10:37:31 -0500

    Added ParseScannerProject

---
+++ ParserScannerOriginal/src/ScannerDemo.java
@@ -0,0 +1,32 @@
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "prog2.jay";
+	private static String file2 = "separator.jay";
+	private static String file3 = "boolean.jay";
+	private static String file4 = "identifier.jay";
+	private static String file5 = "integer.jay";
+	private static String file6 = "keyword.jay";
+	private static String file7 = "literal.jay";
+	private static String file8 = "operator.jay";
+	private static String file9 = "separator.jay";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+
+		TokenStream ts = new TokenStream(file7);
+
+
+		
+		Token tok;
+
+		while (!ts.isEndofFile()) {
+			tok = ts.nextToken();
+			System.out.println(tok.toString());
+		}
+	}
+}
