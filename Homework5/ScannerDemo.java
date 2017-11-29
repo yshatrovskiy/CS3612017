2496f9a327e664cfb2486d0e8182d175c3412015
Yevgeniy <yshatrovskiy@gmail.com>
2017-11-29 11:02:49 -0500

    First Homework Commit

---
+++ Homework5/ScannerDemo.java
@@ -0,0 +1,29 @@
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "keyword.jay";
+	
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+
+		TokenStream ts = new TokenStream(file1);
+		Token tok;
+		int i = 0;
+		while (!ts.isEndofFile()) {
+			
+			if(ts.isEndofFile())
+				break;
+			tok = ts.nextToken();
+			i++;
+//			if(tok.getType().equals("Other"))
+//				System.out.println("Token " + i + " " + tok.toString());
+			System.out.println("Token " + i + " - Type: " + tok.getType() + " - Value: " + tok.getValue());
+		
+		}
+	}
+}
