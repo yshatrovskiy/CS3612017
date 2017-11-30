33e206a1fe39f5f871ddba6b1b875dcbe5ee5805
Yevgeniy <yshatrovskiy@gmail.com>
2017-11-30 16:02:59 -0500

    Fixed While and If

--- ParserScannerOriginal/src/ParserDemo.java
+++ ParserScannerOriginal/src/ParserDemo.java
@@ -4,12 +4,9 @@ public class ParserDemo {
 	private static String file3 = "boolean.jay";
 	public static void main(String[] args) {
 		TokenStream tStream = new TokenStream(file1);
-		System.out.println("test1");
 		ConcreteSyntax cSyntax = new ConcreteSyntax(tStream);
-		System.out.println("test2");
 		Program p = cSyntax.program();
 		System.out.println(p.display());
-		System.out.println("test3");
 	}
 
 }
